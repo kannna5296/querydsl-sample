c4b3cbcea7b4af525a072c76f7f82e4a941edebf
kannna5296 <kannnahacks5296@gmail.com>
2023-05-21 16:44:40 +0900

    QEntity生成OK

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -14,15 +14,10 @@ allOpen {
 dependencies {
     implementation("com.h2database:h2:1.3.148")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.hibernate:hibernate-core:5.6.12.Final")
-    // https://mvnrepository.com/artifact/com.querydsl/querydsl-jpa
+
     kapt("com.querydsl:querydsl-apt:5.0.0:jpa") //ここがQEntity生成のため必須
-    implementation("com.querydsl:querydsl-jpa:5.0.0") //queryDSLのインポート
-//    kapt("com.querydsl:querydsl-core:4.1.3")
-//    "https://mvnrepository.com/artifact/com.querydsl/querydsl-kotlin-codegen")
-    kapt("com.querydsl:querydsl-kotlin-codegen:5.0.0")
-//    // https://mvnrepository.com/artifact/com.querydsl/querydsl-apt
-//    implementation("com.querydsl:querydsl-apt:5.0.0")
+    implementation("com.querydsl:querydsl-jpa:5.0.0") //com.querydsl.jpa.impl.JPAQueryFactory利用のため必須
+
     // infraはドメインに依存
     implementation(project(":domain"))
     implementation(project(":usecase"))
