70b3fa1bcbd351550b0d047caf870750711946d2
kannna5296 <kannnahacks5296@gmail.com>
2023-05-21 16:24:08 +0900

    コミット(import com.querydsl.jpaパッケージがインポートできない)

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -2,12 +2,16 @@ plugins {
     kotlin("kapt") //kotlin用アノテーションプロセッサ(annotationからクラス自動生成したりする)
     //入れてビルドするとbuild/generated/フォルダが映える
 }
+
+apply(plugin = "kotlin-kapt")
+apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
 dependencies {
     implementation("com.h2database:h2:1.3.148")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     // https://mvnrepository.com/artifact/com.querydsl/querydsl-jpa
     kapt("com.querydsl:querydsl-jpa:4.1.3")
-    implementation("com.querydsl:querydsl-core:4.1.3")
+    ("com.querydsl:querydsl-core:4.1.3")
 // // // https://mvnrepository.com/artifact/com.querydsl/querydsl-kotlin-codegen
 //    implementation("com.querydsl:querydsl-kotlin-codegen:5.0.0")
 //    // https://mvnrepository.com/artifact/com.querydsl/querydsl-apt
