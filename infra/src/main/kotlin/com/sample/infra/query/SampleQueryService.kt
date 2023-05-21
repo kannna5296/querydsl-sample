a7e980d85a41a17c0d63c15e2bc957219147d82c
kannna5296 <kannnahacks5296@gmail.com>
2023-05-21 17:22:29 +0900

    QueryService実装

--- infra/src/main/kotlin/com/sample/infra/query/SampleQueryService.kt
+++ infra/src/main/kotlin/com/sample/infra/query/SampleQueryService.kt
@@ -1,7 +1,10 @@
  
 package com.sample.infra.query
 
+import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.sample.infra.jpa.entity.QBookEntity
+
 import com.sample.usecase.query.BookDto
 import com.sample.usecase.query.BookWithRentalDto
 import com.sample.usecase.query.ISampleQueryService
@@ -11,10 +14,19 @@ import org.springframework.stereotype.Repository
 class SampleQueryService(
     private val queryFactory: JPAQueryFactory // TODO こいつの設定
 ) : ISampleQueryService {
-//    val book = com.sample.infra.jpa.entity
+    private val book = QBookEntity.bookEntity //build/generated/source/kapt/main/.../infra.jpa.entityに自動生成される
 
-    override fun findBooksById(id: String): BookDto {
-        TODO("Not yet implemented")
+
+    override fun findBooksById(id: String): BookDto? {
+        val result = queryFactory.select(
+            Projections.constructor(
+                BookDto::class.java,
+                book.id,
+                book.author,
+                book.author,
+            )).from(book).fetchOne()
+        println(result) //簡易ログ
+        return result
     }
 
     override fun findBookWithRentalById(id: String): BookWithRentalDto {
